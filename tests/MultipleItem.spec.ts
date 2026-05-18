51d8bafbf3a6a013f812565e9389fe1972e3765d
Yash <Yashkamti2050@gmail.com>
2026-05-18 17:40:05 +0530

    feat: add multiple orders test with login flow

--- tests/MultipleItem.spec.ts
+++ tests/MultipleItem.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from "../src/page/Login";
 import { testData } from "../src/page/testdata/testdata";
 
-test.only('Multipleorders', async ({ page }) => {
+test('Multipleorders', async ({ page }) => {
     const loginpage = new LoginPage(page);
     await loginpage.loginpageNavigate();
     await loginpage.login(
@@ -14,10 +14,16 @@ test.only('Multipleorders', async ({ page }) => {
     await expect(page.locator('button.spinner-plus').nth(2)).toBeVisible();
     await page.locator('[data-menu-section-id="75976"]').click();
 
-    await page.locator("spinner-plus-btn").nth(2).click();
-    await page.locator("spinner-plus-btn").nth(3).click();
-    await page.locator("spinner-plus-btn").nth(1).click();
+    await page.locator('[data-test-id="increase-quantity-btn"]').nth(1).click();
+    await page.locator('[data-test-id="increase-quantity-btn"]').nth(2).click();
+    await page.locator('[data-test-id="increase-quantity-btn"]').nth(3).click();
+    // await page.locator(".spinner-plus-btn").nth(3).click();
+    // await page.locator(".spinner-plus-btn").nth(1).click();
+    await page.locator ('[data-test-id="payment-btn-cash"]').click();
     await page.getByRole('button', { name: 'PLACE ORDER' }).click();
     await page.getByRole('button', { name: 'Exact amount' }).click();
+    await page.locator ('[data-test-id="orders-button"]').click();
+   
+
 
 });
