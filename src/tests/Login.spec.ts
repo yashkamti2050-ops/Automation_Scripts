403e214d0aea3128cbf07681b19ff3e84931d6a7
Yash <Yashkamti2050@gmail.com>
2026-06-15 12:32:00 +0530

    Login Update

--- src/tests/Login.spec.ts
+++ src/tests/Login.spec.ts
@@ -8,7 +8,7 @@ test.describe('Login', () => {
     await login.loginpageNavigate();
   })
 
-  test.only('Login functionality flow', async ({ login }) => {
+  test('Login functionality flow', async ({ login }) => {
     await login.login_Page();
   })
 
