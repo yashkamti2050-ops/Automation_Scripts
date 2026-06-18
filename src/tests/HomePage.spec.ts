b66c2a36707f96b42eb345a24eeef1de4f9d57d4
Yash <Yashkamti2050@gmail.com>
2026-06-18 16:10:31 +0530

    Update HomePage SpecFile

--- src/tests/HomePage.spec.ts
+++ src/tests/HomePage.spec.ts
@@ -6,27 +6,26 @@ test.describe('Payments Flow', () => {
 
     test.beforeEach('Login and navigate to home page', async ({ login }) => {
         await login.loginPageNavigate();
-
-
-        test('Check Cash Payment flow', async ({ homePage }) => {
-            await homePage.createCashOrder();
-        });
-
-        test('Check Pay later Payment Flow', async ({ homePage }) => {
-
-            await homePage.createLaterOrder();
-        });
-
-        test('Check Voucher payment flow', async ({ homePage }) => {
-            await homePage.createVoucherOrder();
-        });
-
-
-        test.skip('Check Card payment flow', async ({ homePage }) => {
-            await homePage.createCardOrder();
-        });
-
-
-
     });
+
+
+    test('Check Cash Payment flow', async ({ homePage }) => {
+        await homePage.createCashOrder();
+    });
+
+    test('Check Pay later Payment Flow', async ({ homePage }) => {
+
+        await homePage.createLaterOrder();
+    });
+
+    test('Check Voucher payment flow', async ({ homePage }) => {
+        await homePage.createVoucherOrder();
+    });
+
+
+    test.skip('Check Card payment flow', async ({ homePage }) => {
+        await homePage.createCardOrder();
+    });
+
+
 })
