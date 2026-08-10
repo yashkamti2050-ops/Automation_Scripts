8b3ea5ed489b5194df0756060774175d86c87f6b
Yash Kamti <yashkamti2050@gmail.com>
2026-08-10 17:19:45 +0530

    Implemented Api testing flow w.r.t Creating a cash type order creation. (Non-Paid)

--- src/page/OrderManagementPage/OrderManagementPage.ts
+++ src/page/OrderManagementPage/OrderManagementPage.ts
@@ -58,7 +58,7 @@ export class OrderManagementPage {
     }
     async verifyOrderDetailPopup() {
         await this.page.waitForLoadState('domcontentloaded');
-        await this.page.waitForLoadState('networkidle');
+        // await this.page.waitForLoadState('networkidle');
         await expect(this.orderCard).toBeVisible();
         await this.orderCard.click();
         await expect(this.orderModel).toBeVisible();
