a32876a080473ca5d782094fca0a041f5e2de9f2
Yash Kamti <yashkamti2050@gmail.com>
2026-09-17 17:16:20 +0530

    Created Test for Refund , Refactored Create cash order test.

--- src/Modules/tests/orderManagementPage/OrderManagementAPI.spec.ts
+++ src/Modules/tests/orderManagementPage/OrderManagementAPI.spec.ts
@@ -2,22 +2,21 @@ import { test } from '../../../support/fixture/Fixtures';
 import { APITestData } from "../../../support/Datastorage/ApiTestData"
 
 
-test.only("Refund Cash Payment through API", async ({
+test("Refund Cash Payment through API", async ({
     homePageApi,
     orderManagementApi
 }) => {
 
-    // 1. Login
+    
     const auth = await homePageApi.setupAuthenticatedApi();
 
-    // 2. Create order + make cash payment
+
     const cashOrder = await homePageApi.createCashOrder(
         auth.login.apiKey,
         APITestData.Amount,
         APITestData.User_Id
     );
 
-    // 3. Refund the payment
     const refundResponse = await orderManagementApi.refundPayment(
         auth.login.apiKey,
         cashOrder.paymentId,
@@ -25,6 +24,6 @@ test.only("Refund Cash Payment through API", async ({
         APITestData.User_Id
     );
 
-    // 4. Validate refund response
+
     await homePageApi.validateApiResponse(refundResponse);
 });
